9b0f9057c15dcc3e2bec090f4f7a8f353fc57534
Chris O'Shea <christopher.d.oshea@gmail.com>
2017-11-12 17:17:48 -0500

    Problem #2 - Product of other numbers

---
+++ InterviewCakeProblems/InterviewCakeUnitTests/ProductOfOtherNumbersTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterviewCakeProblems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCakeProblems.Tests
+{
+    [TestClass()]
+    public class ProductOfOtherNumbersTests
+    {
+        [TestMethod()]
+        public void GetProductsOfAllIntsExceptAtIndexTest_TestExample()
+        {
+            int[] testArray = new int[] { 1, 7, 3, 4 };
+            int[] expectedArray = new int[] { 84, 12, 28, 21 };
+
+            var tester = new ProductOfOtherNumbers();
+            var result = tester.GetProductsOfAllIntsExceptAtIndex(testArray);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(expectedArray[i], result[i]);
+            }
+            
+        }
+    }
+}
