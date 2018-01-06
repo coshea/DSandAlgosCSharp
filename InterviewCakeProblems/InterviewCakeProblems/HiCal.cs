2965b32f92ae3cd766c54929a1e0f19f18161fe9
Chris O'Shea <christopher.d.oshea@gmail.com>
2018-01-06 15:37:21 -0500

    Question #4 - Calendar meeting time problem

---
+++ InterviewCakeProblems/InterviewCakeProblems/HiCal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCakeProblems
+{
+    public class Meeting
+    {
+        public int StartTime { get; set; }
+
+        public int EndTime { get; set; }
+
+        public Meeting(int startTime, int endTime)
+        {
+            // Number of 30 min blocks past 9:00 am
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        public override string ToString()
+        {
+            return $"({StartTime}, {EndTime})";
+        }
+    }
+
+    public class HiCal
+    {
+        public List<Meeting> MergeRanges(List<Meeting> meetings)
+        {
+            var sortedMeetings = meetings.Select(m => new Meeting(m.StartTime, m.EndTime))
+                .OrderBy(m => m.StartTime).ToList();
+
+            List<Meeting> mergedMeetings = new List<Meeting>() { sortedMeetings[0] };
+
+            foreach (Meeting currentMeeting in sortedMeetings)
+            {
+                var lastMergedMeeting = mergedMeetings.Last();
+
+                if (currentMeeting.StartTime <= lastMergedMeeting.EndTime)
+                {
+                    lastMergedMeeting.EndTime = Math.Max(lastMergedMeeting.EndTime, currentMeeting.EndTime);
+                }
+                else
+                {
+                    mergedMeetings.Add(currentMeeting);
+                }
+            }
+
+            return mergedMeetings;
+        }
+    }
+}
---
+++ InterviewCakeProblems/InterviewCakeUnitTests/Arrays/HiCalTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using InterviewCakeProblems;
+
+namespace InterviewCakeUnitTests.Arrays
+{
+    [TestClass]
+    public class HiCalTests
+    {
+        [TestMethod]
+        public void HiCalMergeRanges_TestExample()
+        {
+            var testCal = new List<Meeting>() { new Meeting(1, 3), new Meeting(2, 4) };
+            var expected = new List<Meeting>() { new Meeting(1, 4) };
+
+            HiCal hiCal = new HiCal();
+            var result = hiCal.MergeRanges(testCal);
+
+            Assert.AreEqual(expected[0].StartTime, result[0].StartTime);
+            Assert.AreEqual(expected[0].EndTime, result[0].EndTime);
+        }
+    }
+}
