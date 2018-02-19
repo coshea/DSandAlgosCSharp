87bbfc2ae98225deed2fbc57860e24ff1be200f4
Chris O <coshea@users.noreply.github.com>
2018-02-18 20:03:36 -0500

    Question #14 - Write a method that takes an integer flightLength (in minutes) and an array of integers movieLengths (in minutes) and returns a boolean indicating whether there are two numbers in movieLengths whose sum equals flightLength.

---
+++ InterviewCakeProblems/InterviewCakeProblems/HashSets/InFlightEntertainment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCakeProblems.HashSets
+{
+    public class InFlightEntertainment
+    {
+        public bool CanTwoMoviesFillFlight(int[] movieLengths, int flightLength)
+        {
+            var movieLengthsSeen = new HashSet<int>();
+
+            foreach (int firstMovieLength in movieLengths)
+            {
+                int matchingSecondMovieLength = flightLength - firstMovieLength;
+                if (movieLengthsSeen.Contains(matchingSecondMovieLength))
+                {
+                    return true;
+                }
+
+                movieLengthsSeen.Add(firstMovieLength);
+            }
+
+            return false;
+        }
+    }
+}
