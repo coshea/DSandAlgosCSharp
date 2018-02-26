25a9e6116ea9bf0e6b36dd2f8b0cbdd4657a9eee
Chris O <coshea@users.noreply.github.com>
2018-02-25 20:32:55 -0500

    Problem #16 - Cake Thief initial checkin

---
+++ InterviewCakeProblems/InterviewCakeProblems/Design/CakeThief.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCakeProblems.Design
+{
+    public class CakeType
+    {
+        public int Weight { get; }
+        public long Value { get; }
+
+        public CakeType(int weight, long value)
+        {
+            Weight = weight;
+            Value = value;
+        }
+    }
+
+    public class CakeThief
+    {
+        public long MaxDuffelBagValue(CakeType[] cakeTypes, int capacity)
+        {
+            return 0;
+        }
+    }
+}
---
+++ InterviewCakeProblems/InterviewCakeUnitTests/Design/CakeThiefTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using InterviewCakeProblems.Design;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InterviewCakeUnitTests.Design
+{
+    [TestClass]
+    public class CakeThiefTests
+    {
+        [TestMethod]
+        public void CakeThief_MaxDuffelBagValue_ExampleProblem()
+        {
+            CakeType[] cakeTypes = new[]
+            {
+                new CakeType(7, 160),
+                new CakeType(3, 90),
+                new CakeType(2, 15),
+            };
+
+            int capacity = 20;
+
+            var cakeThief = new CakeThief();
+
+            // Returns 555 (6 of the middle type of cake and 1 of the last type of cake)
+            var result = cakeThief.MaxDuffelBagValue(cakeTypes, capacity);
+
+            Assert.AreEqual(555, result);
+        }
+    }
+}
