bdd8542305ce1272a7cbe3c2ead502e29793fa16
Chris O <coshea@users.noreply.github.com>
2018-03-04 14:52:53 -0500

    Problem #30 -  Write an efficient method that checks whether any permutation of an input string is a palindrome.

---
+++ InterviewCakeProblems/InterviewCakeProblems/Strings/Palindrome.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCakeProblems.Strings
+{
+    public class Palindrome
+    {
+        /// <summary>
+        /// Problem #30
+        /// Write an efficient method that checks whether any permutation
+        /// of an input string is a palindrome.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public bool HasPalindromePermutation(string input)
+        {
+            // Track characters we've seen an odd number of times
+            HashSet<char> unpairedCharacters = new HashSet<char>();
+
+            foreach (var c in input)
+            {
+                if (unpairedCharacters.Contains(c))
+                {
+                    unpairedCharacters.Remove(c);
+                }
+                else
+                {
+                    unpairedCharacters.Add(c);
+                }
+            }
+
+            // The string has a palindrome permutation if it
+            // has one or zero characters without a pair
+            return unpairedCharacters.Count <= 1;
+        }
+    }
+}
---
+++ InterviewCakeProblems/InterviewCakeUnitTests/Strings/PalindromeTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using InterviewCakeProblems.Strings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InterviewCakeUnitTests.Strings
+{
+    [TestClass]
+    public class PalindromeTests
+    {
+        [TestMethod]
+        public void HasPalindromePermutation_TestExample_Success()
+        {
+            var testString = "civic";
+            var expectedResult = true;
+
+            var palindrome = new Palindrome();
+            var result = palindrome.HasPalindromePermutation(testString);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void HasPalindromePermutation_TestExample_Fail()
+        {
+            var testString = "civil";
+            var expectedResult = false;
+
+            var palindrome = new Palindrome();
+            var result = palindrome.HasPalindromePermutation(testString);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
