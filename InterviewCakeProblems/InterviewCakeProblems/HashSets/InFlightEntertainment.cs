406ceabb85245b4886052f0c3a614a009863b99c
Chris O <coshea@users.noreply.github.com>
2018-03-07 16:31:08 -0500

    Comment update

--- InterviewCakeProblems/InterviewCakeProblems/HashSets/InFlightEntertainment.cs
+++ InterviewCakeProblems/InterviewCakeProblems/HashSets/InFlightEntertainment.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace InterviewCakeProblems.HashSets
 {
+    /// <summary>
+    /// Question #14
+    /// Write a method that takes an integer flightLength (in minutes) 
+    /// and an array of integers movieLengths (in minutes) 
+    /// and returns a boolean indicating whether there are two numbers 
+    /// in movieLengths whose sum equals flightLength.
+    /// </summary>
     public class InFlightEntertainment
     {
         public bool CanTwoMoviesFillFlight(int[] movieLengths, int flightLength)
