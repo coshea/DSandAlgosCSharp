15559eeeeeb6596c23982f916bc174363a682ea6
Chris O <coshea@users.noreply.github.com>
2018-03-17 16:21:37 -0400

    Renaming unit tests

--- InterviewCakeProblems/InterviewCakeUnitTests/Design/LRUCacheTests.cs
+++ InterviewCakeProblems/InterviewCakeUnitTests/Design/LRUCacheTests.cs
@@ -12,7 +12,7 @@ namespace InterviewCakeUnitTests.Design
     public class LRUCacheTests
     {
         [TestMethod()]
-        public void LRUCacheTest_ExampleProblem()
+        public void LRUCacheTest_ExampleProblem1()
         {
             LRUCache cache = new LRUCache(2 /* capacity */ );
 
@@ -43,11 +43,56 @@ namespace InterviewCakeUnitTests.Design
             Assert.AreEqual(2, cache.Get(3));       // returns 2
         }
 
+        [TestMethod()]
+        public void LRUCacheTest_ExampleProblem3()
+        {
+            //["LRUCache","put","put","get","put","get","get"]
+            //[[2],[2,1],[1,1],[2],[4,1],[1],[2]]
+
+            LRUCache cache = new LRUCache(2 /* capacity */ );
+
+            cache.Put(2, 1);
+            cache.Put(1, 1);
+            Assert.AreEqual(1, cache.Get(2));
+
+            cache.Put(4, 1);
+            Assert.AreEqual(-1, cache.Get(1));
+            Assert.AreEqual(1, cache.Get(2));
+
+
+        }
+
+
+        [TestMethod()]
+        public void LRUCacheTest_ExampleProblem4()
+        {
+            //["LRUCache","put","put","put","put","get","get","get","get","put","get","get","get","get","get"]
+            //[[3],[1,1],[2,2],[3,3],[4,4],[4], [3],[2],[1], [5,5],[1],[2],[3],[4],[5]]
+
+            LRUCache cache = new LRUCache(3 /* capacity */ );
+
+            cache.Put(1, 1);
+            cache.Put(2, 2);
+            cache.Put(3, 3);
+            cache.Put(4, 4);
+            Assert.AreEqual(4, cache.Get(4));
+            Assert.AreEqual(3, cache.Get(3));
+            Assert.AreEqual(2, cache.Get(2));
+            Assert.AreEqual(-1, cache.Get(1));
+            cache.Put(5, 5);
+            Assert.AreEqual(-1, cache.Get(1));
+            Assert.AreEqual(2, cache.Get(2));
+            Assert.AreEqual(3, cache.Get(3));
+            Assert.AreEqual(-1, cache.Get(4));
+            Assert.AreEqual(5, cache.Get(5));
+
+        }
+
 
 
 
         [TestMethod()]
-        public void LRUCacheBuiltInTest_ExampleProblem()
+        public void LRUCacheBuiltInTest_ExampleProblem1()
         {
             LRUCacheBuiltIn cache = new LRUCacheBuiltIn(2 /* capacity */ );
 
