bf3c6012a60b84b9bac56933eeb714f387b7dd05
Chris O <coshea@users.noreply.github.com>
2018-03-17 16:21:23 -0400

    HackerRank - Matrix Layer Rotation. CCW and CW rotations

---
+++ InterviewCakeProblems/InterviewCakeProblems/Arrays/MatrixRotation.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCakeProblems.Arrays
+{
+    public static class MatrixRotation
+    {
+        public static int[,] RotateClockwise(int rows, int cols, int[,] matrix)
+        {
+            int[,] rotatedMatrix = matrix;
+            int rowIndex = 0;
+            int colIndex = 0;
+            int rowEnd = rows;
+            int colEnd = cols;
+            int prev = 0;
+            int curr = 0;
+
+
+            while (rowIndex < rowEnd && colIndex < colEnd)
+            {
+                if (rowIndex + 1 == rowEnd || colIndex + 1 == colEnd)
+                {
+                    break;
+                }
+
+                // Store the first element of next row, this
+                // element will replace first element of current row
+                prev = rotatedMatrix[rowIndex + 1, colIndex];
+
+                //Move elements of top row.
+                for (int i = colIndex; i < colEnd; i++)
+                {
+                    curr = rotatedMatrix[rowIndex, i];
+                    rotatedMatrix[rowIndex, i] = prev;
+                    prev = curr;
+                }
+                rowIndex++;
+
+                //Move elements of last column.
+                for (int i = rowIndex; i < rowEnd; i++)
+                {
+                    curr = rotatedMatrix[i, colEnd - 1];
+                    rotatedMatrix[i, colEnd - 1] = prev;
+                    prev = curr;
+                }
+                colEnd--;
+
+                // Move elements of bottom row.
+                if (rowIndex < rowEnd)
+                {
+                    for (int i = colEnd - 1; i >= colIndex; i--)
+                    {
+                        curr = rotatedMatrix[rowEnd - 1, i];
+                        rotatedMatrix[rowEnd - 1, i] = prev;
+                        prev = curr;
+                    }
+                }
+                rowEnd--;
+
+                //Move elements of first column.
+                if (colIndex < colEnd)
+                {
+                    for (int i = rowEnd - 1; i >= rowIndex; i--)
+                    {
+                        curr = rotatedMatrix[i, colIndex];
+                        rotatedMatrix[i, colIndex] = prev;
+                        prev = curr;
+                    }
+                }
+                colIndex++;
+            }
+            return rotatedMatrix;
+        }
+
+        public static int[,] RotateCounterClockwise(int rows, int cols, int[,] matrix)
+        {
+            int[,] rotatedMatrix = matrix;
+            int rowIndex = 0;
+            int colIndex = 0;
+            int rowEnd = rows;
+            int colEnd = cols;
+            int prev = 0;
+            int curr = 0;
+
+            while (rowIndex < rowEnd && colIndex < colEnd)
+            {
+                if (rowIndex + 1 == rowEnd || colIndex + 1 == colEnd)
+                {
+                    break;
+                }
+
+                prev = rotatedMatrix[rowIndex, colIndex + 1];
+
+                // Move first column
+                for (int i = rowIndex; i < rowEnd; i++)
+                {
+                    curr = rotatedMatrix[i, colIndex];
+                    rotatedMatrix[i, colIndex] = prev;
+                    prev = curr;
+                }
+                colIndex++;
+
+                //Move bottom row
+                for (int i = colIndex; i < colEnd; i++)
+                {
+                    curr = rotatedMatrix[rowEnd - 1, i];
+                    rotatedMatrix[rowEnd - 1, i] = prev;
+                    prev = curr;
+                }
+                rowEnd--;
+
+                // Move farthest right column
+                if (colIndex < colEnd)
+                {
+                    for (int i = rowEnd - 1; i >= rowIndex; i--)
+                    {
+                        curr = rotatedMatrix[i, colEnd - 1];
+                        rotatedMatrix[i, colEnd - 1] = prev;
+                        prev = curr;
+                    }
+
+                }
+                colEnd--;
+
+                // Move top most row
+                if (rowIndex < rowEnd)
+                {
+                    for (int i = colEnd - 1; i >= colIndex; i--)
+                    {
+                        curr = rotatedMatrix[rowIndex, i];
+                        rotatedMatrix[rowIndex, i] = prev;
+                        prev = curr;
+                    }
+                }
+                rowIndex++;
+
+            }
+
+            return rotatedMatrix;
+        }
+
+    }
+}
---
+++ InterviewCakeProblems/InterviewCakeUnitTests/Arrays/MatrixRotationTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterviewCakeProblems.Arrays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCakeProblems.Arrays
+{
+    [TestClass()]
+    public class MatrixRotationTests
+    {
+        [TestMethod()]
+        public void RotateClockwiseTest()
+        {
+            int[,] testArray = new int[4, 4]
+            {   { 1,2,3,4 },
+                { 5,6,7,8 },
+                { 9,10,11,12 },
+                { 13,14,15,16 }};
+
+            var expectedArray = new int[4, 4]
+            {   { 5,1,2,3, },
+                { 9,10,6,4 },
+                { 13,11,7,8 },
+                { 14,15,16,12 }};
+
+            var result = MatrixRotation.RotateClockwise(4, 4, testArray);
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(result[i, j] + " ");
+                    Assert.AreEqual(expectedArray[i, j], result[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        [TestMethod()]
+        public void RotateCounterClockwiseTest()
+        {
+            int[,] testArray = new int[4, 4]
+            {   { 1,2,3,4 },
+                { 5,6,7,8 },
+                { 9,10,11,12 },
+                { 13,14,15,16 }};
+
+            var expectedArray = new int[4, 4]
+            {   { 2,3,4,8 },
+                { 1,7,11,12 },
+                { 5,6,10,16 },
+                { 9,13,14,15 }};
+
+            var result = MatrixRotation.RotateCounterClockwise(4, 4, testArray);
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(result[i, j] + " ");
+                    Assert.AreEqual(expectedArray[i, j], result[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
