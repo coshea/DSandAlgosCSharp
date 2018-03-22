b1bef432685440fa249053a1c80ceb7dc0aba2c5
Chris O <coshea@users.noreply.github.com>
2018-03-21 22:06:48 -0400

    Namespace updates

--- InterviewCakeProblems/InterviewCakeUnitTests/StacksAndQueues/MinStackTests.cs
+++ InterviewCakeProblems/InterviewCakeUnitTests/StacksAndQueues/MinStackTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InterviewCakeProblems.StacksAndQueues
+namespace InterviewCakeUnitTests.StacksAndQueues
 {
     [TestClass()]
     public class MinStackTests
