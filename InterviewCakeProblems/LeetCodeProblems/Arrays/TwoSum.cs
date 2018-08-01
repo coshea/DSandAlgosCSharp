e4f00fe513b00ee2929b06e0cbcd7732bac3f70a
Chris O <coshea@users.noreply.github.com>
2018-07-31 21:39:39 -0400

    Leetcode #1 Two Sums

---
+++ InterviewCakeProblems/LeetCodeProblems/Arrays/TwoSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Arrays
+{
+    public class TwoSum
+    {
+        public int[] GetTwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> sums = new Dictionary<int, int>();
+
+            for (int x = 0; x < nums.Length; x++)
+            {
+                var diff = target - nums[x];
+                if (sums.ContainsKey(diff))
+                {
+                    return new int[] { sums[diff], x };
+                }
+
+                if (!sums.ContainsKey(nums[x]))
+                    sums.Add(nums[x], x);
+            }
+
+            return null;
+        }
+    }
+}
