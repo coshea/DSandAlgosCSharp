bfaf593395b680e3cefe50f93a5a9af5b1631f52
Chris O <coshea@users.noreply.github.com>
2018-08-11 15:58:05 -0400

    Leetcode #186 - Reverse Words in a String II

---
+++ InterviewProblems/LeetCodeProblems/Strings/ReverseWordsInAString.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Strings
+{
+    public class ReverseWordsInAString
+    {
+        /// <summary>
+        /// Leetcode #186 - Reverse Words in a String II
+        /// Given an input string , reverse the string word by word. 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public char[] ReverseWords(char[] str)
+        {
+            // 1 Reverse whole string
+            Reverse(str, 0, str.Length - 1);
+
+            int start = 0;
+            // 2 Reverse each word
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    Reverse(str, start, i - 1);
+                    start = i + 1;
+                }
+            }
+
+            // 3 Reverse last word
+            Reverse(str, start, str.Length - 1);
+
+            return str;
+
+        }
+
+        private void Reverse(char[] str, int start, int end)
+        {
+            while (start < end)
+            {
+                var tmp = str[end];
+                str[end] = str[start];
+                str[start] = tmp;
+
+                start++;
+                end--;
+            }
+        }
+
+    }
+}
---
+++ InterviewProblems/LeetCodeProblemsTests/Strings/ReverseWordsInAStringTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeProblems.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Strings.Tests
+{
+    [TestClass()]
+    public class ReverseWordsInAStringTests
+    {
+        [TestMethod()]
+        public void ReverseWordsTest()
+        {
+            char[] str = { 't', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e' };
+            char[] expected = { 'b', 'l', 'u', 'e', ' ', 'i', 's', ' ', 's', 'k', 'y', ' ', 't', 'h', 'e' };
+
+            var tester = new ReverseWordsInAString();
+            CollectionAssert.AreEqual(expected, tester.ReverseWords(str));
+        }
+    }
+}
