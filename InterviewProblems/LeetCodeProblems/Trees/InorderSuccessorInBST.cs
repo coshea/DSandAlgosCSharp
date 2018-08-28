6765c1d5d6e58e60bf3fc01b1366044f14da69da
Chris O <coshea@users.noreply.github.com>
2018-08-27 20:31:03 -0400

    Another garbage question. Didn't answer this question. Answered a question to get the preceeding root node instead

---
+++ InterviewProblems/LeetCodeProblems/Trees/InorderSuccessorInBST.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Trees
+{
+    /// <summary>
+    /// Leetcode 285. Inorder Successor in BST
+    /// https://leetcode.com/problems/inorder-successor-in-bst/description/
+    /// iven a binary search tree and a node in it, find the in-order successor of that node in the BST.
+    /// Note: If the given node has no in-order successor in the tree, return null.
+    /// </summary>
+    /// <remarks>Solution doesn't answer this question</remarks>
+    public class InorderSuccessorInBST
+    {
+        public TreeNode InorderSuccessor(TreeNode root, TreeNode p)
+        {
+            Stack<TreeNode> searchStack = new Stack<TreeNode>();
+            Stack<TreeNode> predStack = new Stack<TreeNode>();
+
+            searchStack.Push(root);
+            predStack.Push(null);
+
+            while (searchStack.Count > 0)
+            {
+                var node = searchStack.Pop();
+                var predNode = predStack.Pop();
+
+                if (node.val == p.val)
+                {
+                    return predNode;
+                }
+
+                if (node.left != null)
+                {
+                    searchStack.Push(node.left);
+                    predStack.Push(node);
+                }
+                if (node.right != null)
+                {
+                    searchStack.Push(node.right);
+                    predStack.Push(node);
+                }
+            }
+
+            return null;
+        }
+    }
+}
