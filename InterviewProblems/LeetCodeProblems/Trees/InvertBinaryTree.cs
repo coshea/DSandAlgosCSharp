309f5df361257ddef191a44f9a5b1354d408bf24
Chris O <coshea@users.noreply.github.com>
2018-09-04 21:10:33 -0400

    Leetcode 226. Invert Binary Tree

---
+++ InterviewProblems/LeetCodeProblems/Trees/InvertBinaryTree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Trees
+{
+    /// <summary>
+    /// Leetcode 226. Invert Binary Tree
+    /// https://leetcode.com/problems/invert-binary-tree/description/
+    /// Invert a binary tree.
+    /// </summary>
+    public class InvertBinaryTree
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+
+            Invert(root);
+
+            return root;
+        }
+
+        private void Invert(TreeNode root)
+        {
+            if (root.left == null && root.right == null)
+            {
+                return;
+            }
+
+            var tmp = root.left;
+            root.left = root.right;
+            root.right = tmp;
+
+            if (root.left != null)
+            {
+                Invert(root.left);
+            }
+            if (root.right != null)
+            {
+                Invert(root.right);
+            }
+        }
+    }
+}
