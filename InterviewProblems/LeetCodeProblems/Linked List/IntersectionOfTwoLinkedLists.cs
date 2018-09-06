f0faec8a7a1d9968db3d88dcec5406a6a1392263
Chris O <coshea@users.noreply.github.com>
2018-09-05 21:25:23 -0400

    Leetcode 160. Intersection of Two Linked Lists

--- InterviewProblems/LeetCodeProblems/Linked List/IntersectionOfTwoLinkedLists.cs
+++ InterviewProblems/LeetCodeProblems/Linked List/IntersectionOfTwoLinkedLists.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace LeetCodeProblems.Linked_List
 {
+    /// <summary>
+    /// Leetcode 160. Intersection of Two Linked Lists
+    /// https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+    /// Write a program to find the node at which the intersection of two singly linked lists begins.
+    /// </summary>
     public class IntersectionOfTwoLinkedLists
     {
         public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
@@ -16,8 +21,54 @@ namespace LeetCodeProblems.Linked_List
                 return null;
             }
 
+            ListNode p1 = headA;
+            ListNode p2 = headB;
 
+            int len1 = 0;
+            int len2 = 0;
 
+            // Get the length of the two lists.
+            while (p1 != null || p2 != null)
+            {
+                if (p1 != null)
+                {
+                    len1++;
+                    p1 = p1.next;
+                }
+                if (p2 != null)
+                {
+                    len2++;
+                    p2 = p2.next;
+                }
+            }
+
+            p1 = headA;
+            p2 = headB;
+
+            // Align them to the same start point.
+            while (len1 < len2)
+            {
+                p2 = p2.next;
+                len2--;
+            }
+
+            while (len2 < len1)
+            {
+                p1 = p1.next;
+                len1--;
+            }
+
+            // Move them together until finding the intersection point, or the end null
+            while (p1 != null || p2 != null)
+            {
+                if (p1 == p2)
+                {
+                    return p1;
+                }
+
+                p1 = p1.next;
+                p2 = p2.next;
+            }
 
             return null;
         }
---
+++ InterviewProblems/LeetCodeProblemsTests/Linked List/IntersectionOfTwoLinkedListsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeProblems.Linked_List;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Linked_List.Tests
+{
+    [TestClass()]
+    public class IntersectionOfTwoLinkedListsTests
+    {
+        [TestMethod()]
+        public void GetIntersectionNodeTest()
+        {
+            ListNode l1 = new ListNode(2);
+            l1.next = new ListNode(4);
+            l1.next.next = new ListNode(3);
+            l1.next.next.next = new ListNode(7);
+
+            ListNode l2 = new ListNode(5);
+            l2.next = new ListNode(6);
+            l2.next.next = new ListNode(4);
+            l2.next.next.next = l1.next.next;
+
+            var tester = new IntersectionOfTwoLinkedLists();
+            var result = tester.GetIntersectionNode(l1, l2);
+        }
+    }
+}
