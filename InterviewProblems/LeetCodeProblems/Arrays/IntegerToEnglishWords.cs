c10484640485204612ab87eecd8d680b38859dfb
Chris O <coshea@users.noreply.github.com>
2018-09-11 21:50:37 -0400

    Leetcode 273. Integer to English Words WIP

--- InterviewProblems/LeetCodeProblems/Arrays/IntegerToEnglishWords.cs
+++ InterviewProblems/LeetCodeProblems/Arrays/IntegerToEnglishWords.cs
@@ -6,49 +6,117 @@ using System.Threading.Tasks;
 
 namespace LeetCodeProblems.Arrays
 {
+    /// <summary>
+    /// Leetcode 273. Integer to English Words
+    /// https://leetcode.com/problems/integer-to-english-words/description/
+    /// Convert a non-negative integer to its english words representation. 
+    /// Given input is guaranteed to be less than 2^31 - 1.
+    /// </summary>
     public class IntegerToEnglishWords
     {
 
-        /// <summary>
-        /// 273. Integer to English Words
-        /// </summary>
-        /// <param name="num"></param>
-        /// <returns></returns>
+        private Dictionary<int, string> wordMap = new Dictionary<int, string>()
+        {
+            { 0, ""},
+           { 1, "One"},
+           { 2, "Two"},
+           { 3, "Three"},
+           { 4, "Four"},
+           { 5, "Five"},
+           { 6, "Six"},
+           { 7, "Seven"},
+           { 8, "Eight"},
+           { 9, "Nine"},
+           { 10, "Ten"},
+           { 11, "Eleven"},
+           { 12, "Twelve"},
+           { 13, "Thirteen"},
+           { 14, "Fourteen"},
+           { 15, "Fifteen"},
+           { 16, "Sixteen"},
+           { 17, "Seventeen"},
+           { 18, "Eightteen"},
+           { 19, "Nineteen"},
+           { 20, "Twenty"},
+           { 30, "Thirty"},
+           { 40, "Forty"},
+           { 50, "Fifty"},
+           { 60, "Sixty"},
+           { 70, "Seventy"},
+           { 80, "Eighty"},
+           { 90, "Ninety"},
+        };
+
+
         public string NumberToWords(int num)
         {
-            //In complete
-            return IntToWord(num);
+            if (num == 0)
+            {
+                return "Zero";
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+
+            Helper(num, sb);
+
+
+            return sb.ToString();
+
         }
 
-
-        private string IntToWord(int num)
+        private void Helper(int num, StringBuilder sb)
         {
-            switch (num)
+
+            if (num >= 1000000000)
             {
-                case 0:
-                    return "";
-                case 1:
-                    return "One";
-                case 2:
-                    return "Two";
-                case 3:
-                    return "Three";
-                case 4:
-                    return "Four";
-                case 5:
-                    return "Five";
-                case 6:
-                    return "Six";
-                case 7:
-                    return "Seven";
-                case 8:
-                    return "Eight";
-                case 9:
-                    return "Nine";
-                default:
-                    return "";
+                //billion
+                sb.Append(wordMap[num / 1000000000]);
+                sb.Append(" Billion ");
+                Helper(num % 1000000000, sb);
+            }
+
+            else if (num >= 1000000)
+            {
+                //million
+                sb.Append(wordMap[num / 1000000]);
+                sb.Append(" Million ");
+                Helper(num % 1000000, sb);
+            }
+
+            else if (num >= 1000)
+            {
+                //thousand
+                Helper(num / 1000, sb);
+                sb.Append(" Thousand ");
+                Helper(num % 1000, sb);
+            }
+
+            else if (num >= 100)
+            {
+                //hundred
+                sb.Append(wordMap[num / 100]);
+                sb.Append(" Hundred ");
+                Helper(num % 100, sb);
+            }
+
+            else if (num >= 10)
+            {
+                //tens
+                sb.Append(wordMap[num / 10 * 10]);
+                sb.Append(" ");
+                Helper(num % 10, sb);
+            }
+
+            else if (num > 0)
+            {
+                //ones
+                sb.Append(wordMap[num]);
             }
 
         }
+
+
+
     }
 }
---
+++ InterviewProblems/LeetCodeProblemsTests/Arrays/IntegerToEnglishWordsTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeProblems.Arrays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Arrays.Tests
+{
+    [TestClass()]
+    public class IntegerToEnglishWordsTests
+    {
+        [TestMethod()]
+        public void NumberToWordsTest_123()
+        {
+            int input = 123;
+            string expected = "One Hundred Twenty Three";
+
+            var tester = new IntegerToEnglishWords();
+
+            Assert.AreEqual(expected, tester.NumberToWords(input));
+        }
+
+        [TestMethod()]
+        public void NumberToWordsTest_12345()
+        {
+            int input = 12345;
+            string expected = "Twelve Thousand Three Hundred Forty Five";
+
+            var tester = new IntegerToEnglishWords();
+
+            Assert.AreEqual(expected, tester.NumberToWords(input));
+        }
+
+        [TestMethod()]
+        public void NumberToWordsTest_1234567()
+        {
+            int input = 1234567;
+            string expected = "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven";
+
+            var tester = new IntegerToEnglishWords();
+
+            Assert.AreEqual(expected, tester.NumberToWords(input));
+        }
+
+        [TestMethod()]
+        public void NumberToWordsTest_1234567891()
+        {
+            int input = 1234567891;
+            string expected = "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One";
+
+            var tester = new IntegerToEnglishWords();
+
+            Assert.AreEqual(expected, tester.NumberToWords(input));
+        }
+    }
+}
