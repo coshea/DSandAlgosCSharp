95bd1bf20664441cc6dcb84d5d23e323059e7ca8
Chris O <coshea@users.noreply.github.com>
2019-11-10 12:21:40 -0500

    Leetcode #20. Valid Parentheses

---
+++ InterviewProblems/LeetCodeProblems/StacksAndQueues/ValidParentheses.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.StacksAndQueues
+{
+    /// <summary>
+    /// Leetcode #20. Valid Parentheses
+    /// </summary>
+    class ValidParentheses
+    {
+        Dictionary<char, char> opens = new Dictionary<char, char>()
+    {
+        {'(', ')'},
+        {'{', '}'},
+        {'[', ']'}
+
+    };
+
+        Dictionary<char, char> closes = new Dictionary<char, char>()
+    {
+        {')', '('},
+        {'}', '{'},
+        {']', '['}
+    };
+
+        Stack<char> openStack = new Stack<char>();
+        Stack<char> closeStack = new Stack<char>();
+
+        public bool IsValid(string s)
+        {
+            foreach (char c in s)
+            {
+                if (opens.ContainsKey(c))
+                    openStack.Push(c);
+                else if (closes.ContainsKey(c))
+                {
+                    if (openStack.Count == 0)
+                        return false;
+                    var left = openStack.Pop();
+                    var right = closes[c];
+                    if (left != right)
+                        return false;
+                }
+            }
+
+            return openStack.Count == 0;
+        }
+    }
+}
